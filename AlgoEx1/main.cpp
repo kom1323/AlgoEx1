891230c1dd58e43e7a889075192e4321212ca2ba
Netanel Kuchma <nati190819@icloud.com>
2023-04-12 10:44:45 +0300

    Last update of directed graph

--- AlgoEx1/main.cpp
+++ AlgoEx1/main.cpp
@@ -9,7 +9,17 @@ int main(void)
 	int n, m,edgeFrom,edgeTo;
 	cout << "Is the graph directed: y/n\n";
 	cin >> choice;
+	if (choice != 'y' && choice != 'n')
+	{
+		cout << "invalid error";
+		exit(1);
+	}
 	cin >> n >> m;
+	if (n <= 0 || m < 0)
+	{
+		cout << "invalid error";
+		exit(1);
+	}
 	switch (choice)
 	{
 	case 'y':
@@ -18,13 +28,15 @@ int main(void)
 	case 'n':
 		graph = new DirectedGraph(n);
 		break;
-	default:
-		exit(1);
-		break;
 	}
 	for (int i = 0; i < m; i++)
 	{
 		cin >> edgeFrom >> edgeTo;
+		if ((edgeFrom<0 && edgeFrom>n) || (edgeTo<0 && edgeTo>n))
+		{
+			cout << "invalid error";
+			exit(1);
+		}	
 		graph->addEdge(edgeFrom, edgeTo);
 	}
 	graph->Euler();
